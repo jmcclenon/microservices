1fa28a2abb6d863645aae3d55cfe6b5d4c5f60fd
jmcclenon <mars.john2018@gmail.com>
2020-11-22 11:07:52 -0500

    Added code comments to the comments/index.js file

--- comments/index.js
+++ comments/index.js
@@ -1,3 +1,7 @@
+// jshint esversion:6
+// jshint linter enabled in Visual Studio Code
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
@@ -5,12 +9,15 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 
+// store your comments in the following array
 const commentsByPostId = {};
 
+//route to get comments and id
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
+// route to post comments, create id
 app.post('/posts/:id/comments', (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
@@ -21,9 +28,11 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  //send back 201-created
   res.status(201).send(comments);
 });
 
+// listen on port 4001
 app.listen(4001, () => {
   console.log('Listening on 4001');
 });
