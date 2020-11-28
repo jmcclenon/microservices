902c2373f1633524bc04b33df74de30f7cd67b5e
jmcclenon <mars.john2018@gmail.com>
2020-11-28 08:27:13 -0500

    Code from the next few lessons in Udemy course

--- comments/index.js
+++ comments/index.js
@@ -5,9 +5,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // store your comments in the following array
 const commentsByPostId = {};
